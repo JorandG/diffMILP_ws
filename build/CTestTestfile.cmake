ddf8b2437a29df0572f1c0b06051c42f2dbf8e7e
JorandG <91953623+JorandG@users.noreply.github.com>
2023-08-23 15:52:11 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jorand/diff_ws/src
-# Build directory: /home/jorand/diff_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("mastering_ros_robot_description_pkg")
-subdirs("diff_wheeled_robot_gazebo")
